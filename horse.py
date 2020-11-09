6107f81a2bba9df248fda9be3962972a11a7c750
Tommy Granström <tommygranstrom@MacBook-Pro-som-tillhor-Tommy.local>
2020-11-09 11:20:07 +0100

    Final commit

--- horse.py
+++ horse.py
@@ -8,31 +8,19 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
-
-
-
-
 import pathlib
-dataset_url = "/Users/tommygranstrom/Downloads/flower_photos 2/"
+
+#Set variables
+ant = "500" #Dummie variable
+epochs = 5 
+dataset_url = "/Users/tommygranstrom/Desktop/AI-Projekt Hästar/Antal/"+ant
 data_dir = pathlib.Path(dataset_url)
-
-# image_count = len(list(data_dir.glob('*/*.jpg')))
-# print(image_count)
-
-# roses = list(data_dir.glob('roses/*'))
-# PIL.Image.open(str(roses[0])).show()
-
-# PIL.Image.open(str(roses[1])).show()
-
-# tulips = list(data_dir.glob('tulips/*'))
-# PIL.Image.open(str(tulips[0])).show()
-
-# PIL.Image.open(str(tulips[1])).show()
 
 batch_size = 32
 img_height = 180
 img_width = 180
 
+#Create training dataset
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
   data_dir,
   validation_split=0.2,
@@ -40,7 +28,7 @@ train_ds = tf.keras.preprocessing.image_dataset_from_directory(
   seed=123,
   image_size=(img_height, img_width),
   batch_size=batch_size)
-
+#Create validation dataset
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
   data_dir,
   validation_split=0.2,
@@ -49,69 +37,71 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
+#Load and print class names (Folders in directory)
 class_names = train_ds.class_names
 print(class_names)
 
 import matplotlib.pyplot as plt
-
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.title(class_names[labels[i]])
-    plt.axis("off")
-plt.show()
 
 for image_batch, labels_batch in train_ds:
   print(image_batch.shape)
   print(labels_batch.shape)
   break
 
-
+#Preprocessing
 AUTOTUNE = tf.data.experimental.AUTOTUNE
-
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
-
 normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
-
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
 first_image = image_batch[0]
 # Notice the pixels values are now in `[0,1]`.
 print(np.min(first_image), np.max(first_image)) 
-
-
 num_classes = 3
 
+#If using data augmentation
+
+# data_augmentation = keras.Sequential(
+#   [
+#     layers.experimental.preprocessing.RandomFlip("horizontal", 
+#                                                  input_shape=(img_height, 
+#                                                               img_width,
+#                                                               3)),
+#     layers.experimental.preprocessing.RandomRotation(0.1),
+#     layers.experimental.preprocessing.RandomZoom(0.1),
+#   ]
+# )
+
+#Create the model
 model = Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  #data_augmentation, #If augmentation
+  layers.experimental.preprocessing.Rescaling(1./255),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
+  #layers.Dropout(0.2), #If using dropout
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
 ])
-
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-model.summary()
+#model.summary()
 
-epochs = 15
+#Fit the model
 history = model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=epochs
 )
 
+#Set variables and plot accuracy and loss
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
@@ -120,7 +110,7 @@ val_loss = history.history['val_loss']
 
 epochs_range = range(epochs)
 
-plt.figure(figsize=(8, 8))
+plt.figure(figsize=(7, 7))
 plt.subplot(1, 2, 1)
 plt.plot(epochs_range, acc, label='Training Accuracy')
 plt.plot(epochs_range, val_acc, label='Validation Accuracy')
@@ -132,23 +122,80 @@ plt.plot(epochs_range, loss, label='Training Loss')
 plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
-plt.show()
+#plt.show() #Plot the figure
+plt.savefig('plottar/'+str(epochs)+'epoker'+ant+'bilder.png') #Save plot
 
-# sunflower_url = "/Users/tommygranstrom/Documents/TNM108/images/test/6.jpg"
-#sunflower_path = tf.keras.utils.get_file('6', origin=sunflower_url)
+#If you want to save the model and dont need to create it again
+# #save the model
+# model.save('saved_model/my_model') 
+import os
+import csv
 
-#sunflower_path = "/Users/tommygranstrom/Documents/TNM108/images/test/6.jpg"
-sunflower_path = "/Users/tommygranstrom/Downloads/flower_photos 2/daisy/dg_00902.png"
-img = keras.preprocessing.image.load_img(
-    sunflower_path, target_size=(img_height, img_width)
-)
-img_array = keras.preprocessing.image.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0) # Create a batch
+#Function that predict predicts a given image
+def predict(path,answer,pathToFile):
+  tobePredicted_path = path
+  img = keras.preprocessing.image.load_img(
+      tobePredicted_path, target_size=(img_height, img_width)
+  )
 
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
+  img_array = keras.preprocessing.image.img_to_array(img)
+  img_array = tf.expand_dims(img_array, 0) # Create a batch
 
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
+  predictions = model.predict(img_array)
+  score = tf.nn.softmax(predictions[0])
+  f = open(pathToFile,"a")
+  f.write(answer+','+str(format(class_names[np.argmax(score)]))+','+str(100 * np.max(score))+'\n')
+  f.close()
+  #Print result
+  print(
+      "This image most likely belongs to {} with a {:.2f} percent confidence."
+      .format(class_names[np.argmax(score)], 100 * np.max(score))
+  )
+
+  # if format(class_names[np.argmax(score)] == answer:
+  #   return [1,100 * np.max(score)]
+
+  # return score
+
+#Paths to all test images  
+pathTrolle="/Users/tommygranstrom/Desktop/AI-Projekt Hästar/Testbilder/Trolle test"
+pathAmmie="/Users/tommygranstrom/Desktop/AI-Projekt Hästar/Testbilder/Ammie"
+pathEffie="/Users/tommygranstrom/Desktop/AI-Projekt Hästar/Testbilder/Effie"
+
+
+#Dummie string to create textfile containing predictions of all test images
+pathfile = "Predictions/"+str(epochs)+'epoker'+ant+'bilder.txt'
+f = open(pathfile,"w")
+f.write('Horse,Prediction,Confidence\n')
+f.close()
+
+#List to auto predict all test images for all three horses
+listTrolle = os.listdir(pathTrolle)    
+listAmmie = os.listdir(pathAmmie)
+listEffie = os.listdir(pathEffie)
+corrCounter = 0
+
+#Predict horses and print result
+print('\nPredict trolle\n')
+for file in listTrolle:
+  if file!='.DS_Store':
+    patH = pathTrolle + '/' + file
+    predict(patH,"Trolle",pathfile)
+
+print('\n----------------\n')
+
+
+print('\nPredict Ammie\n')
+for file in listAmmie:
+  if file!='.DS_Store':
+    patH = pathAmmie + '/' + str(file)
+    predict(patH,"Ammie",pathfile)
+print('\n----------------\n')
+corrCounter = 0
+
+print('\nPredict Effie\n')
+for file in listEffie:
+  if file!='.DS_Store':
+    patH = pathEffie + '/' + str(file)
+    predict(patH,"Effie",pathfile)
+print('\n----------------\n')
